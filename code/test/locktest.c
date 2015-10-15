46aee1762e58051ae5c4ff52815b1963fb0bda8d
jamestlynch <lynch.james.thomas@gmail.com>
2015-10-14 23:57:34 -0700

    Add skeleton for all lock tests.

--- code/test/locktest.c
+++ code/test/locktest.c
@@ -4,20 +4,57 @@
 
 #include "syscall.h"
 
+void
+CreateLock_Test() {
+    // If length is 0
+
+    // Bad vaddr
+
+    
+
+    // If vector has no more room ??
+
+    // Memory running out ??
+}
+
+void
+AcquireLock_Test() {
+    // CreateLock
+
+	// Invalid indeces: negative, out of bounds
+
+	// Lock is set to delete
+
+	// Process is not lock owner
+
+}
+
+void 
+ReleaseLock_Test() {
+	// Invalid indeces: negative, out of bounds
+
+	// Lock is set to delete
+
+	// Process is not lock owner
+}
+
+void
+DestroyLock_Test() {
+	// Invalid indeces: negative, out of bounds
+
+	// Process is not lock owner
+
+	// No waiting threads: delete
+
+	// Waiting threads: don't delete but set to be deleted
+}
+
 int main() {
-  OpenFileId fd;
-  int bytesread;
-  char buf[20];
-
-    Create("testfile", 8);
-    fd = Open("testfile", 8);
-
-    Write("testing a locktest write\n", 16, fd );
-    Close(fd);
+    CreateLock_Test();
+    AcquireLock_Test();
+    ReleaseLock_Test();
+    DestroyLock_Test();
 
 
-    fd = Open("testfile", 8);
-    bytesread = Read( buf, 100, fd );
-    Write( buf, bytesread, ConsoleOutput );
-    Close(fd);
+	// Multiple threads test
 }
