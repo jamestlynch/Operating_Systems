6188fc6ac2e22f0d647dadbd2f7309d6d8e4f453
jamestlynch <lynch.james.thomas@gmail.com>
2015-10-20 11:41:25 -0700

    Replace Write with WriteError in locktest.

--- code/test/locktest.c
+++ code/test/locktest.c
@@ -10,34 +10,34 @@ CreateLock_Test() {
 
     indexlock1 = CreateLock("abc", 0);
     if (indexlock1 != -1) {
-    	Write("CreateLock failed: Should return -1 when the length of the lock's identifier is 0.\n", 83, 1);
+    	WriteError("CreateLock failed: Should return -1 when the length of the lock's identifier is 0.\n", 83);
     }
 
     indexlock1 = CreateLock("abc", -1);
     if (indexlock1 != -1) {
-        Write("CreateLock failed: Should return -1 for negative lock identifier lengths.\n", 74, 1);
+        WriteError("CreateLock failed: Should return -1 for negative lock identifier lengths.\n", 74);
     }
 
     indexlock1 = CreateLock(0, 1);
     if (indexlock1 != -1) {
-        Write("CreateLock failed: Should return -1 for bad pointers to lock identifier.\n", 73, 1);
+        WriteError("CreateLock failed: Should return -1 for bad pointers to lock identifier.\n", 73);
     }
-
     indexlock1 = CreateLock(-1, 1);
     if (indexlock1 != -1) {
-        Write("CreateLock failed: Should return -1 for invalid pointers to lock identifier.\n", 77, 1);
+        WriteError("CreateLock failed: Should return -1 for invalid pointers to lock identifier.\n", 77);
     }
 
     indexlock1 = CreateLock("abc", 3);
     if (indexlock1 == -1) {
-        Write("CreateLock failed: Should NOT return -1 when valid lock identifier and lengths are passed in.\n", 94, 1);
+        WriteError("CreateLock failed: Should NOT return -1 when valid lock identifier and lengths are passed in.\n", 94);
     }
+
+        WriteInt(indexlock1);
 
     indexlock2 = CreateLock("abc", 3);
     if (indexlock1 == indexlock2) {
-        Write("CreateLock failed: Should NOT return the same index when creating two locks.\n", 77, 1);
+        WriteError("CreateLock failed: Should NOT return the same index when creating two locks.\n", 77);
     }
-
 
     /* Possible other tests: */
     /*  - Bad vaddr: different address space */
