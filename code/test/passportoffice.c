fcd74ff8ea9393cc5a9532582461468b6b7583c8
jamestlynch <lynch.james.thomas@gmail.com>
2015-10-20 16:24:27 -0700

    Add senator data (same structure as customer) and senator data initialization function.

--- code/test/passportoffice.c
+++ code/test/passportoffice.c
@@ -25,7 +25,9 @@ int customerData[7] = {
 };
 
 int numCustomers = 50;
-int *customers[50][7]; /* Array of customers, indexed by ssn (Must support at least 50 customers) */
+int customers[50][7]; /* Array of customers, indexed by ssn (Must support at least 50 customers) */
+int numSenators = 10;
+int senators[10][7]; /* Array of customers, indexed by ssn (Must support at least 50 customers) */
 
 void CreateCustomerData ()
 {
@@ -40,10 +42,59 @@ void CreateCustomerData ()
 	}
 }
 
+void CreateSenatorData ()
+{
+	int ssn, i;
+
+	for (ssn = numCustomers; ssn < (numCustomers + numSenators); ssn++)
+	{
+		for (i = 0; i < numCustomerDataEntries; i++)
+		{
+			senators[ssn][i] = customerData[i];
+		}
+	}
+}
+
+/******************************************/
+/* 			    Clerk Data 				  */
+/******************************************/
+
+int state_clerkAvailable = 0;
+int state_clerkBusy = 1;
+int state_clerkOnBreak = 2;
+
+int index_state = 0;
+int index_currentCustomer = 1;
+int index_money = 2;
+int index_lineCount = 3;
+int index_bribeLineCount = 4;
+int index_senatorLineCount = 5;
+int index_isBeingBribed = 6;
+int index_customerLikedPhoto = 7;
+int index_customerAppReadyToCertify = 8;
+int index_customerAppReadyForPayment = 9;
+
+int numClerkDataEntries = 10;
+
+int clerkData[] = {
+	0, /* state */
+	-1, /* currentCustomer */
+	0, /* money */
+	0, /* lineCount */
+	0, /* bribeLineCount */
+	0, /* senatorLineCount */
+	0, /* isBeingBribed */
+	0, /* customerLikedPhoto */
+	0, /* customerAppReadyToCertify */
+	0 /* customerAppReadyForPayment */
+};
+
+int numAppClerks = 5;
+
 void InitializeData ()
 {
-
 	CreateCustomerData();
+	CreateSenatorData();
 }
 
 int main() 
