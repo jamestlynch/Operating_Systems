6567c889f1dcdae9d466086697175276136141e9
jamestlynch <lynch.james.thomas@gmail.com>
2015-11-10 18:45:16 -0800

    Add MV test.

--- code/test/clientTest.c
+++ code/test/clientTest.c
@@ -9,7 +9,30 @@ void ClientTest(){
 
 	Write("Client test index: %d \n",indexcheck1, 1);
 }
+
+void MVTest()
+{
+	int indexmv, mvvalue;
+
+	indexmv = CreateMV("MVTest", sizeof("MVTest"), 4); /* Create a MV with ID "MVTest" and size 4 */
+	SetMV(indexmv, 0, 4); /* Set MV[0] = 4 */
+	mvvalue = GetMV(indexmv, 0); /* Should == 4 */
+
+	if (mvvalue != 4)
+	{
+		PrintfOne("Failed [CreateAndUpdateMV] MV[0] should equal 4. Instead, equals %d",
+			sizeof("Failed [CreateAndUpdateMV] MV[0] should equal 4. Instead, equals %d"),
+			mvvalue);
+		Halt();
+	}
+	else
+	{
+		PrintfOne("Passed [CreateAndUpdateMV]");
+	}
+}
+
 int main() 
 {
-    ClientTest();
+    /*ClientTest();*/
+    MVTest();
 }
