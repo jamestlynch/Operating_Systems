79576fc8f18ce299b236e87bfc8aed725e6bd2aa
hlenner <lenner@usc.edu>
2015-12-02 19:00:27 -0800

    Start of Project 4

---
+++ code/test/Exec2.c
@@ -0,0 +1,15 @@
+#include "syscall.h"
+
+int main()
+{
+	Exec("../test/customer", sizeof("../test/customer"));
+/*	Exec("../test/customer", sizeof("../test/customer"));
+	Exec("../test/customer", sizeof("../test/customer"));
+	Exec("../test/customer", sizeof("../test/customer"));
+	Exec("../test/customer", sizeof("../test/customer"));
+	Exec("../test/customer", sizeof("../test/customer"));
+	Exec("../test/customer", sizeof("../test/customer"));
+	Exec("../test/customer", sizeof("../test/customer"));
+	Exec("../test/customer", sizeof("../test/customer"));
+	Exec("../test/customer", sizeof("../test/customer"));*/
+}
